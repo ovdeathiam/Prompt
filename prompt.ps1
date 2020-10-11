cb9a5b305e81068373065be873cf7efde2563ba5
Ovdeathiam <krystian.maksymowicz@gmail.com>
2020-10-11 20:18:52 +0200

    Removed rightside timer due to resize window bug.

--- prompt.ps1
+++ prompt.ps1
@@ -1,12 +1,13 @@
 function prompt {
-    $Title      = [Environment]::MachineName + " @ " + [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
-    $UpperLeft  = $ExecutionContext.SessionState.Path.CurrentLocation.ProviderPath
-    $UpperRight = (Get-Date).ToString('HH:mm:ss') + "`n"
+    $Title = [Environment]::MachineName + " @ " + [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+    $Path  = $ExecutionContext.SessionState.Path.CurrentLocation.ProviderPath
+    $Timer = (Get-Date).ToString('HH:mm:ss')
 
     $host.ui.RawUI.WindowTitle = $Title
-    Write-Host -NoNewLine -Object $UpperLeft -ForegroundColor Green
-    $host.ui.Rawui.CursorPosition = @{ X = $($host.ui.rawui.MaxWindowSize.Width - $UpperRight.Length); Y= $($host.ui.Rawui.CursorPosition.y)};
-    Write-Host -NoNewline -Object $UpperRight -ForegroundColor Yellow
+    Write-Host -NoNewline -Object $($Timer + " ") -ForegroundColor Yellow
+    Write-Host <#-NoNewLine #>-Object $Path #-ForegroundColor Green
+    # $host.ui.Rawui.CursorPosition = @{ X = $($host.ui.rawui.MaxWindowSize.Width - $UpperRight.Length); Y= $($host.ui.Rawui.CursorPosition.y)};
+    # Write-Host -NoNewline -Object $($Timer + "`n") -ForegroundColor Yellow
 
     Write-Host -NoNewLine -Object "PS"
     Write-Host -NoNewline -Object $PSVersionTable.PSVersion.Major, $PSVersionTable.PSVersion.Minor -Separator "." -ForegroundColor DarkCyan
